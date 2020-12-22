1e48256993653ce4744a79906c252d7f5c280925
ankitap09 <ankitap09@users.noreply.github.com>
2020-12-22 16:52:52 +0530

    first commit

---
+++ src/main/java/com/capgemini/fitnessappsprint2/FitnessAppSprint2Application.java
@@ -0,0 +1,13 @@
+package com.capgemini.fitnessappsprint2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAppSprint2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAppSprint2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/fitnessappsprint2/FitnessAppSprint2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.fitnessappsprint2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppSprint2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
